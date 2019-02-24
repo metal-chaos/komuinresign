b278b6d5a74db3e809ba6694d181060cda18c65a
metal-chaos <nightrage.metal66@gmail.com>
2019-02-24 21:08:48 +0900

    change function to add cache date

--- functions.php
+++ functions.php
@@ -74,11 +74,11 @@ if (!is_admin()) {
 		wp_register_script( 'remodal', get_bloginfo('template_directory'). '/library/js/libs/remodal.js', array('jquery'), '1.0.0', true );
 		wp_register_script( 'masonry.pkgd.min', get_bloginfo('template_directory'). '/library/js/libs/masonry.pkgd.min.js', array('jquery'), '4.0.0', true );
 		wp_register_script( 'imagesloaded', get_bloginfo('template_directory'). '/library/js/libs/imagesloaded.js', array('jquery'), '4.1.0', true );
+		wp_register_script( 'glitch', get_bloginfo('template_directory'). '/library/js/libs/imagesloaded.pkgd.min.js', array(), '', true );
 		if(!wp_is_mobile() && !strstr($ieua, 'Trident') && !strstr($ieua, 'MSIE') && (get_option('side_options_animatenone') == "ani_on")){
 			wp_register_script( 'wow', get_bloginfo('template_directory'). '/library/js/libs/wow.min.js', array('jquery'), '', true );
 		}
 		wp_register_script( 'main-js', get_bloginfo('template_directory'). '/library/js/scripts.js', array( 'jquery' ), '', true );
-		wp_register_script( 'glitch', get_bloginfo('template_directory'). '/library/js/imagesloaded.pkgd.min.js', array(), '', true );
 	}
 	function add_script() {
 		register_script();
@@ -103,7 +103,7 @@ if (!is_admin()) {
 CSS読み込み
 *********************/
 function register_style() {
-	wp_register_style('style', get_bloginfo('template_directory').'/style.css');
+	wp_register_style('style', get_bloginfo('template_directory').'/style.css?20190225');
 	wp_register_style('shortcode', get_bloginfo('template_directory').'/library/css/shortcode.css');
 	wp_register_style('fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css');
 	wp_register_style('gf_Concert', '//fonts.googleapis.com/css?family=Concert+One');
@@ -481,57 +481,3 @@ add_filter( 'comment_form_default_fields', function ( $fields ) {
 
 	return $fields;
 } );
-
-/*********************
-OGPタグ/Twitterカード設定を出力
-*********************/
-function my_meta_ogp() {
-	if( is_front_page() || is_home() || is_singular() ){
-	  global $post;
-	  $ogp_title = '';
-	  $ogp_descr = '';
-	  $ogp_url = '';
-	  $ogp_img = '';
-	  $insert = '';
-  
-	  if( is_singular() ) { //記事＆固定ページ
-		 setup_postdata($post);
-		 $ogp_title = $post->post_title;
-		 $ogp_descr = mb_substr(get_the_excerpt(), 0, 100);
-		 $ogp_url = get_permalink();
-		 wp_reset_postdata();
-	  } elseif ( is_front_page() || is_home() ) { //トップページ
-		 $ogp_title = get_bloginfo('name');
-		 $ogp_descr = get_bloginfo('description');
-		 $ogp_url = home_url();
-	  }
-  
-	  //og:type
-	  $ogp_type = ( is_front_page() || is_home() ) ? 'website' : 'article';
-  
-	  //og:image
-	  if ( is_singular() && has_post_thumbnail() ) {
-		 $ps_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
-		 $ogp_img = $ps_thumb[0];
-	  } else {
-	   $ogp_img = 'http://komuinresign.net/blog/wp-content/uploads/2017/08/6064f7787c40a8bc472963f79703170b.jpg';
-	  }
-  
-	  //出力するOGPタグをまとめる
-	  $insert .= '<meta property="og:title" content="'.esc_attr($ogp_title).'" />' . "\n";
-	  $insert .= '<meta property="og:description" content="'.esc_attr($ogp_descr).'" />' . "\n";
-	  $insert .= '<meta property="og:type" content="'.$ogp_type.'" />' . "\n";
-	  $insert .= '<meta property="og:url" content="'.esc_url($ogp_url).'" />' . "\n";
-	  $insert .= '<meta property="og:image" content="'.esc_url($ogp_img).'" />' . "\n";
-	  $insert .= '<meta property="og:site_name" content="'.esc_attr(get_bloginfo('name')).'" />' . "\n";
-	  $insert .= '<meta name="twitter:card" content="summary_large_image" />' . "\n";
-	  $insert .= '<meta name="twitter:site" content="@Yutti_Onioni" />' . "\n";
-	  $insert .= '<meta property="og:locale" content="ja_JP" />' . "\n";
-  
-	  //facebookのapp_id（設定する場合）
-  
-	  echo $insert;
-	}
-  } //END my_meta_ogp
-  
-  add_action('wp_head','my_meta_ogp');//headにOGPを出力
